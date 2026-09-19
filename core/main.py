b6ab4534a5eb4e94c52d509c533bbdbd5d2660ec
ttmodupe-hash <253524603+ttmodupe-hash@users.noreply.github.com>
2026-09-19 07:08:50 +0200

    feat(payments): mount /v1/payments/stripe router

--- core/main.py
+++ core/main.py
@@ -69,6 +69,7 @@ from .automation_engine import automation_router
 from .voice_api import router as voice_router
 from .pedagogy_engine import pedagogy_router
 from .companion_engine import companion_router
+from .stripe_payments import stripe_router
 from .sovereign_core import sovereign_router
 from .integrity_engine import integrity_router
 from .universal_learning import universal_router
@@ -360,6 +361,7 @@ app.include_router(automation_router)
 app.include_router(voice_router)
 app.include_router(pedagogy_router)
 app.include_router(companion_router)
+app.include_router(stripe_router)
 app.include_router(sovereign_router)
 app.include_router(integrity_router)
 app.include_router(universal_router)
